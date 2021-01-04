492e2f2f7b922de635865fa1c77058e28203e71b
Ranganath Venkataraman <rangys3@gmail.com>
2021-01-04 01:37:22 +0000

    Add files via upload

---
+++ score_regr.py
@@ -0,0 +1,22 @@
+import json
+import pandas as pd
+import os
+import joblib, pickle
+from azureml.core import Model
+
+
+def init():
+    global daone
+    model_path = Model.get_model_path('registered_regr.sav')
+    daone = joblib.load(model_path)
+
+def run(data):
+    try:
+        trynn = json.loads(data)
+        data = pd.DataFrame(trynn['data'])
+        result = daone.predict(data)
+        # You can return any data type, as long as it is JSON serializable.
+        return result.tolist()
+    except Exception as e:
+        error = str(e)
+        return error
